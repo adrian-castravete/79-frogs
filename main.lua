738355ff4e86156974f60b72400572794d2a0e7c
Adrian Castravete <adrian.castravete@gmail.com>
2021-05-13 00:24:36 +0300

    Add score (WIP)

--- main.lua
+++ main.lua
@@ -28,6 +28,11 @@ C{
 		lg.rotate(e.r)
 		lg.scale(e.s, e.s)
 		lg.translate(-e.w*0.5, -e.h*0.5)
+		if e.color then
+			lg.setColor(e.color)
+		else
+			lg.setColor(1, 1, 1)
+		end
 		e.draw(e)
 		lg.pop()
 	end,
@@ -52,27 +57,42 @@ local LillyBody = {
 	name = "collision",
 }
 
-function LillyBody:init()
-	local b = lp.newBody(fkge.find("world").world, self.x, self.y, "dynamic")
-	local s = lp.newCircleShape(8 * self.s)
-	local f = lp.newFixture(b, s, self.s)
-	b:setPosition(self.x, self.y)
-	b:setLinearVelocity(0, 0)
-	f:setRestitution(0.4)
-	self.body = b
-	self.shape = s
-	self.fixture = f
-end
-
 function LillyBody:system(msg, dt)
+	if not self.body then
+		local b = lp.newBody(fkge.find("world").world, self.x, self.y, "dynamic")
+		local sh = lp.newCircleShape(8 * self.s)
+		local f = lp.newFixture(b, sh)
+		b:setPosition(self.x, self.y)
+		b:setLinearVelocity(0, 0)
+		b:setLinearDamping(0.4, 0.4)
+		--f:setRestitution(0.4)
+		self.body = b
+		self.shape = sh
+		self.fixture = f
+	end
 	self.x, self.y = self.body:getPosition()
 end
 
 function LillyBody:setSpeed(a, v)
-	local vx = math.sin(a) * v * 1000
-	local vy = -math.cos(a) * v * 1000
-	self.body:applyForce(vx, vy)
+	local vx = math.sin(a) * v * 100
+	local vy = -math.cos(a) * v * 100
+	self.body:setLinearVelocity(vx, vy)
 end
+
+--[[
+function LillyBody:_draw()
+	local x, y = self.body:getPosition()
+	local r = self.shape:getRadius()
+	lg.circle("line", x, y, r)
+end
+
+local oldDraw = love.draw
+function love.draw()
+	oldDraw()
+	fkge.each("collision", function(e)
+		e:_draw()
+	end)
+end--]]
 C(LillyBody)
 
 local lillyImg = lg.newImage("lilly.png")
@@ -150,6 +170,7 @@ C{
 		e.y = l.y
 		e.rd = l.r - e.r
 		e.setState(e, "ready")
+		fkge.message("flower", "landedOn", l)
 	end,
 	setState = function (e, s)
 		e.state = s
@@ -219,9 +240,106 @@ C{
 				e.doJump(e)
 			end
 			e.r = e.lilly.r - e.rd
+			e.x = e.lilly.x
+			e.y = e.lilly.y
 		end
 	end,
 }
+
+local Flower = {
+	name = "flower",
+	parents = "2d",
+	image = lillyImg,
+	quad = lg.newQuad(16, 0, 8, 8, lillyImg:getDimensions()),
+}
+
+function Flower:init(lilly)
+	local d = math.random() * 0.1
+	self.color = {1, 1 - d - math.random() * 0.1, 1 - d, 0}
+	self.s = 0
+	local s = 1 + math.random()
+	A(1, function (v)
+		self.color[4] = v
+		self.s = s * v
+	end, function ()
+		self.color[4] = nil
+		self.s = s * v
+	end)
+	self.lilly = lilly
+	self:updatePos()
+end
+
+function Flower:system(msg)
+	self:updatePos()
+	for _, o in ipairs(msg.landedOn or {}) do
+		if o == self.lilly then
+			self.destroy = true
+			fkge.message("score", "flowerTaken", 1)
+		end
+	end
+end
+
+function Flower:updatePos()
+	local l = self.lilly
+	self.x = l.x
+	self.y = l.y
+	self.r = l.r
+end
+
+C(Flower)
+
+local scoreImg = lg.newImage("score.png")
+local sw, sh = scoreImg:getDimensions()
+local scoreQuads = {}
+for i=1, 10 do
+	local ii = i - 1
+	local sx = ii % 4
+	local sy = math.floor(ii / 4)
+	scoreQuads[i] = lg.newQuad(sx*16, sy*16, 16, 16, sw, sh)
+end
+local Score = {
+	name = "score",
+	parents = "2d",
+	value = 0,
+	w = 16,
+	h = 16,
+	image = scoreImg,
+	quad = scoreQuads[1],
+}
+
+function Score:system(msg)
+	for _, s in ipairs(msg.flowerTaken or {}) do
+		self.value = self.value + s
+	end
+	self.placeOver = true
+end
+
+function Score:draw()
+	local o = love.window.getDisplayOrientation()
+	o = (o == "portrait" or o == "portraitflipped")
+	local v = self.value
+	local x = 0
+	while v >= 10 do
+		self:drawDigit(v%10, x, o)
+		v = math.floor(v/10)
+		x = x + 1
+	end
+	self:drawDigit(v, x, o)
+end
+
+function Score:drawDigit(n, x, o)
+	lg.push()
+	lg.translate(-x*16, 0)
+	if o then
+		lg.translate(16, 16)
+		lg.rotate(-math.pi/2)
+		lg.translate(0, -16)
+	end
+	lg.draw(scoreImg, scoreQuads[n+1], 0, 0)
+	lg.pop()
+end
+
+C(Score)
 
 local World = {
 	name = "world",
@@ -229,10 +347,10 @@ local World = {
 
 function World:init()
 	self.world = lp.newWorld()
-	self:createBound(0, -24, WIDTH, 24)
-	self:createBound(0, HEIGHT, WIDTH, 24)
-	self:createBound(-24, -24, 24, HEIGHT+48)
-	self:createBound(WIDTH, -24, 24, HEIGHT+48)
+	self:createBound(WIDTH/2, -16, WIDTH, 32)
+	self:createBound(WIDTH/2, HEIGHT+16, WIDTH, 32)
+	self:createBound(-16, HEIGHT/2, 32, HEIGHT+64)
+	self:createBound(WIDTH+16, HEIGHT/2, 32, HEIGHT+64)
 end
 
 function World:createBound(x, y, w, h)
@@ -242,6 +360,16 @@ function World:createBound(x, y, w, h)
 end
 
 function World:system(msg, dt)
+	if fkge.count("flower") < 3 then
+		fkge.each("lilly", function (e)
+			if math.random() < 0.1 and 
+			   not fkge.find("froggy", function (o)
+							return o.lilly == e
+						end) then
+				E("flower", e)
+			end
+		end)
+	end
 	self.world:update(dt)
 end
 C(World)
@@ -268,6 +396,10 @@ S("game", function ()
 		end
 	end
 	E("froggy")
+	E("score").attr{
+		x = WIDTH - 16,
+		y = 16,
+	}
 end)
 
 fkge.game{
